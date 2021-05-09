f5faa8a78a1705aff05358f26b009b51538e27cb
Renzo Crisostomo <renzo@crisostomo.me>
2021-05-09 11:25:34 +0200

    Add clang-tidy configuration for libsakura target
    Disable readability-magic-numbers because we'll use a lot of those
    when dealing with MMIO ranges

--- src/sakura/src/Processor.cpp
+++ src/sakura/src/Processor.cpp
@@ -25,6 +25,5 @@ void Processor::initialize(const std::filesystem::path &rom) {
 
 void Processor::fetch_instruction() {
   m_registers.program_counter.value++;
-  m_mapping_controller->store(0x1FFF, // NOLINT(readability-magic-numbers)
-                              0xAA);  // NOLINT(readability-magic-numbers)
+  m_mapping_controller->store(0x1FFF, 0xAA);
 }
