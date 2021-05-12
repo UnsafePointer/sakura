d124e31e6e1ad92ea27df0b5c3e3b8094c25bf69
Renzo Crisostomo <renzo@crisostomo.me>
2021-05-12 18:38:21 +0200

    Add Bits namespace to libcommon

---
+++ src/common/src/Bits.cpp
@@ -0,0 +1,17 @@
+#include "common/Bits.hpp"
+
+auto Common::Bits::test_power_of_2(int power_of_2) -> int {
+  int bit_position = 0;
+  for (;;) {
+    if (power_of_2 == 0) {
+      return -1;
+    }
+    int test_bit = power_of_2 & 0b1;
+    if (test_bit != 0) {
+      break;
+    }
+    power_of_2 >>= 1;
+    bit_position += 1;
+  }
+  return bit_position;
+}
