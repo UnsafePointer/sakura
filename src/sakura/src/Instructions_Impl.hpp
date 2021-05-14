c2a59de55f28e2ee1deac635a96c894770b222f4
Renzo Crisostomo <renzo@crisostomo.me>
2021-05-14 11:18:20 +0200

    Fix LDA (ABS, IMM), TAMi and AND instructions status register state

--- src/sakura/src/Instructions_Impl.hpp
+++ src/sakura/src/Instructions_Impl.hpp
@@ -20,8 +20,9 @@ auto Sakura::HuC6280::SEI(std::unique_ptr<Processor> &processor) -> uint8_t {
 
 template <>
 auto Sakura::HuC6280::CSL(std::unique_ptr<Processor> &processor) -> uint8_t {
-  processor->m_registers.status.memory_operation = 0;
   processor->m_speed = Speed::Low;
+
+  processor->m_registers.status.memory_operation = 0;
   return 3;
 }
 
@@ -32,6 +33,10 @@ auto Sakura::HuC6280::LDA_IMM(std::unique_ptr<Processor> &processor)
       processor->m_registers.program_counter.value);
   processor->m_registers.program_counter.value += 1;
   processor->m_registers.accumulator = imm;
+
+  processor->m_registers.status.negative = (imm >> 7) & 0b1;
+  processor->m_registers.status.memory_operation = 0;
+  processor->m_registers.status.zero = imm == 0;
   return 2;
 }
 
@@ -51,6 +56,7 @@ auto Sakura::HuC6280::TAM_I(std::unique_ptr<Processor> &processor) -> uint8_t {
   processor->m_mapping_controller->set_mapping_register(
       bit_position, processor->m_registers.accumulator);
 
+  processor->m_registers.status.memory_operation = 0;
   return 5;
 }
 
@@ -68,6 +74,9 @@ auto Sakura::HuC6280::LDA_ABS(std::unique_ptr<Processor> &processor)
   uint8_t value = processor->m_mapping_controller->load(address);
   processor->m_registers.accumulator = value;
 
+  processor->m_registers.status.negative = (value >> 7) & 0b1;
+  processor->m_registers.status.memory_operation = 0;
+  processor->m_registers.status.zero = value == 0;
   return 5;
 }
 
@@ -81,7 +90,12 @@ auto Sakura::HuC6280::AND_IMM(std::unique_ptr<Processor> &processor)
   uint8_t imm = processor->m_mapping_controller->load(
       processor->m_registers.program_counter.value);
   processor->m_registers.program_counter.value += 1;
-  processor->m_registers.accumulator = processor->m_registers.accumulator & imm;
+  uint8_t result = processor->m_registers.accumulator & imm;
+  processor->m_registers.accumulator = result;
+
+  processor->m_registers.status.negative = (result >> 7) & 0b1;
+  processor->m_registers.status.memory_operation = 0;
+  processor->m_registers.status.zero = result == 0;
   return 2;
 }
 
