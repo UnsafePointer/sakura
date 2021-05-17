7b71fcde4bcea7975fdffeb1ee8d8d4299774363
Renzo Crisostomo <renzo@crisostomo.me>
2021-05-17 19:16:09 +0200

    Implement LDA (ABS, Y)

--- src/sakura/src/Instructions_Impl.hpp
+++ src/sakura/src/Instructions_Impl.hpp
@@ -613,4 +613,26 @@ auto Sakura::HuC6280::NOP(std::unique_ptr<Processor> &processor, uint8_t opcode)
   return 2;
 }
 
+template <>
+auto Sakura::HuC6280::LDA_ABS_Y(std::unique_ptr<Processor> &processor,
+                                uint8_t opcode) -> uint8_t {
+  (void)opcode;
+  uint16_t ll = processor->m_mapping_controller->load(
+      processor->m_registers.program_counter.value);
+  processor->m_registers.program_counter.value += 1;
+  uint16_t hh = processor->m_mapping_controller->load(
+      processor->m_registers.program_counter.value);
+  processor->m_registers.program_counter.value += 1;
+
+  uint16_t address = hh << 8 | ll;
+  processor->m_registers.accumulator =
+      processor->m_mapping_controller->load(address + processor->m_registers.y);
+
+  processor->m_registers.status.negative =
+      (processor->m_registers.accumulator >> 7) & 0b1;
+  processor->m_registers.status.memory_operation = 0;
+  processor->m_registers.status.zero = processor->m_registers.accumulator == 0;
+  return 5;
+}
+
 #endif
--- src/sakura/src/Processor.hpp
+++ src/sakura/src/Processor.hpp
@@ -154,6 +154,8 @@ private:
   friend auto CLY(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   template <typename T>
   friend auto NOP(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
+  template <typename T>
+  friend auto LDA_ABS_Y(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   // clang-format on
 
 public:
