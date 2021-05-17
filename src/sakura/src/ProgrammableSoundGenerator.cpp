3b91b66dbf73f85864a880c317a84801319ed397
Renzo Crisostomo <renzo@crisostomo.me>
2021-05-17 12:16:08 +0200

    Add log message for unimplemented HuC6280 PSG channel registers

--- src/sakura/src/ProgrammableSoundGenerator.cpp
+++ src/sakura/src/ProgrammableSoundGenerator.cpp
@@ -10,6 +10,16 @@ auto Controller::load(uint16_t offset) const -> uint8_t {
     return m_channel_select.value;
   case 0b0001:
     return m_main_amplitude_level_adjustment.value;
+  case 0b0010:
+  case 0b0011:
+  case 0b0100:
+  case 0b0101:
+  case 0b0110:
+  case 0b0111:
+    std::cout << Common::Formatter::format(
+                     "Unimplemented HuC6280 PSG load with offset: %04x", offset)
+              << std::endl;
+    return 0xFF;
   case 0b1000:
     return m_low_frequency_oscillator_frequency;
   case 0b1001:
@@ -29,6 +39,17 @@ void Controller::store(uint16_t offset, uint8_t value) {
     break;
   case 0b0001:
     m_main_amplitude_level_adjustment.value = value;
+    break;
+  case 0b0010:
+  case 0b0011:
+  case 0b0100:
+  case 0b0101:
+  case 0b0110:
+  case 0b0111:
+    std::cout << Common::Formatter::format("Unimplemented HuC6280 PSG store "
+                                           "with offset: %04x, value: %02x",
+                                           offset, value)
+              << std::endl;
     break;
   case 0b1000:
     m_low_frequency_oscillator_frequency = value;
