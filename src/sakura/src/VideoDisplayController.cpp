a0c2a5f2c7edec8e9c06106ebeecb7af15377d19
Renzo Crisostomo <renzo@crisostomo.me>
2021-05-23 12:09:52 +0200

    Improve logging in Sakura::HuC6270::Controller class

--- src/sakura/src/VideoDisplayController.cpp
+++ src/sakura/src/VideoDisplayController.cpp
@@ -4,9 +4,58 @@
 
 using namespace Sakura::HuC6270;
 
+auto REGISTER_SYMBOL_FOR_ADDRESS(uint8_t address) -> std::string {
+  switch (address) {
+  case 0b00000:
+    return "MAWR";
+  case 0b00001:
+    return "MARR";
+  case 0b00010:
+    return "VWR";
+  case 0b00011:
+    return "RESERVED";
+  case 0b00100:
+    return "RESERVED";
+  case 0b00101:
+    return "CR";
+  case 0b00110:
+    return "RCR";
+  case 0b00111:
+    return "BXR";
+  case 0b01000:
+    return "BYR";
+  case 0b01001:
+    return "MWR";
+  case 0b01010:
+    return "HSR";
+  case 0b01011:
+    return "HDR";
+  case 0b01100:
+    return "VPR";
+  case 0b01101:
+    return "VDW";
+  case 0b01110:
+    return "VCR";
+  case 0b01111:
+    return "DCR";
+  case 0b10000:
+    return "SOUR";
+  case 0b10001:
+    return "DESR";
+  case 0b10010:
+    return "LENR";
+  case 0b10011:
+    return "DVSSR";
+  default:
+    return "";
+  }
+}
+
 auto Controller::load(uint16_t offset) const -> uint8_t {
   switch (offset & 0b11) {
   case 0b00:
+    spdlog::get(LOGGER_NAME)
+        ->info(fmt::format("[L] [{:^7}] [xx]: {:#04x}", "SR", m_status.value));
     return m_status.value;
   default:
     spdlog::get(LOGGER_NAME)
@@ -19,21 +68,21 @@ auto Controller::load(uint16_t offset) const -> uint8_t {
 void Controller::store(uint16_t offset, uint8_t value) {
   switch (offset & 0b11) {
   case 0b00:
+    spdlog::get(LOGGER_NAME)
+        ->info(fmt::format("[S] [{:^7}] [xx]: {:#04x}", "AR", value));
     m_address.value = value;
     break;
   case 0b10:
     spdlog::get(LOGGER_NAME)
-        ->warn(fmt::format(
-            "Unhandled HuC6270 store (0b10) with selected address: {:#04x}, "
-            "value: {:#04x}",
-            m_address.value, value));
+        ->info(fmt::format("[S] [{:^7}] [ll]: {:#04x}",
+                           REGISTER_SYMBOL_FOR_ADDRESS(m_address.address),
+                           value));
     break;
   case 0b11:
     spdlog::get(LOGGER_NAME)
-        ->warn(fmt::format(
-            "Unhandled HuC6270 store (0b11) with selected address: {:#04x}, "
-            "value: {:#04x}",
-            m_address.value, value));
+        ->info(fmt::format("[S] [{:^7}] [hh]: {:#04x}",
+                           REGISTER_SYMBOL_FOR_ADDRESS(m_address.address),
+                           value));
     break;
   default:
     spdlog::get(LOGGER_NAME)
