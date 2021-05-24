aaf5757fe61afbd6b0cd7e0bc9a259eb2a9d4aed
Renzo Crisostomo <renzo@crisostomo.me>
2021-05-24 17:45:03 +0200

    Improve logging in Sakura::HuC6260::Controller class

--- src/sakura/src/VideoColorEncoder.cpp
+++ src/sakura/src/VideoColorEncoder.cpp
@@ -5,26 +5,41 @@
 using namespace Sakura::HuC6260;
 
 auto Controller::load(uint16_t offset) const -> uint8_t {
-  (void)offset;
-  (void)m_color_table_address;
-  return 0xFF;
+  (void)m_control;
+  switch (offset) {
+  default:
+    spdlog::get(LOGGER_NAME)
+        ->critical(
+            fmt::format("Unhandled HuC6260 load with offset: {:#06x}", offset));
+    exit(1); // NOLINT(concurrency-mt-unsafe)
+  }
 }
 
 void Controller::store(uint16_t offset, uint8_t value) {
   switch (offset & 0b111) {
   case 0b000:
+    spdlog::get(LOGGER_NAME)
+        ->info(fmt::format("[S] [{:^7}] [ll]: {:#04x}", "CR", value));
     m_control = value;
     break;
   case 0b010:
+    spdlog::get(LOGGER_NAME)
+        ->info(fmt::format("[S] [{:^7}] [ll]: {:#04x}", "CTA", value));
     m_color_table_address.low = value;
     break;
   case 0b011:
+    spdlog::get(LOGGER_NAME)
+        ->info(fmt::format("[S] [{:^7}] [hh]: {:#04x}", "CTA", value));
     m_color_table_address.high = value;
     break;
   case 0b100:
+    spdlog::get(LOGGER_NAME)
+        ->info(fmt::format("[S] [{:^7}] [ll]: {:#04x}", "CTW", value));
     m_color_table_data_write.low = value;
     break;
   case 0b101:
+    spdlog::get(LOGGER_NAME)
+        ->info(fmt::format("[S] [{:^7}] [hh]: {:#04x}", "CTW", value));
     m_color_table_data_write.high = value;
     break;
   default:
