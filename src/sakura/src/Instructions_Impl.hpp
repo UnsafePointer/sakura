5c469c990146288de3202d28cd646f6804e391fa
Renzo Crisostomo <renzo@crisostomo.me>
2021-10-03 10:49:41 +0200

    Fix another (IND, Y) bug

--- src/sakura/src/Instructions_Impl.hpp
+++ src/sakura/src/Instructions_Impl.hpp
@@ -1025,10 +1025,8 @@ auto Sakura::HuC6280::LDA_IND_Y(std::unique_ptr<Processor> &processor,
   processor->m_registers.program_counter.value += 1;
 
   uint16_t zp_address = 0x2000 | zz;
-  uint16_t ll = processor->m_registers.accumulator +
-                processor->m_mapping_controller->load(zp_address);
-  uint16_t hh = processor->m_registers.accumulator +
-                processor->m_mapping_controller->load(zp_address + 1);
+  uint16_t ll = processor->m_mapping_controller->load(zp_address);
+  uint16_t hh = processor->m_mapping_controller->load(zp_address + 1);
 
   uint16_t address = hh << 8 | ll;
   address += processor->m_registers.y;
@@ -1290,10 +1288,8 @@ auto Sakura::HuC6280::STA_IND_Y(std::unique_ptr<Processor> &processor,
   processor->m_registers.program_counter.value += 1;
 
   uint16_t zp_address = 0x2000 | zz;
-  uint16_t ll = processor->m_registers.accumulator +
-                processor->m_mapping_controller->load(zp_address);
-  uint16_t hh = processor->m_registers.accumulator +
-                processor->m_mapping_controller->load(zp_address + 1);
+  uint16_t ll = processor->m_mapping_controller->load(zp_address);
+  uint16_t hh = processor->m_mapping_controller->load(zp_address + 1);
 
   uint16_t address = hh << 8 | ll;
   address += processor->m_registers.y;
