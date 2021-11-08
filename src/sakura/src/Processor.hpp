5785df65acdffadaf7b05f8d8dfb1bf7a3209236
Renzo Crisostomo <renzo@crisostomo.me>
2021-11-08 18:55:58 +0100

    Implement CMP (IND, Y)

--- src/sakura/src/Processor.hpp
+++ src/sakura/src/Processor.hpp
@@ -394,6 +394,8 @@ private:
   friend auto CPY_ZP(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   template <typename T>
   friend auto CPY_ABS(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
+  template <typename T>
+  friend auto CMP_IND_Y(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   // clang-format on
 
 public:
