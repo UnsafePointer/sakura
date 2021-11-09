cc3f89f7e3b3fceccde5dabc2ce923e758bb59cf
Renzo Crisostomo <renzo@crisostomo.me>
2021-11-09 20:28:00 +0100

    Implement LSR (ZP, X)

--- src/sakura/src/Processor.hpp
+++ src/sakura/src/Processor.hpp
@@ -420,6 +420,8 @@ private:
   friend auto ROL_ABS(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   template <typename T>
   friend auto ROR_ABS(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
+  template <typename T>
+  friend auto LSR_ZP_X(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   // clang-format on
 
 public:
