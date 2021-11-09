94222ede096178c2ed23b6338e9b778d98edffdb
Renzo Crisostomo <renzo@crisostomo.me>
2021-11-09 20:32:56 +0100

    Implement ROL (ZP, X)

--- src/sakura/src/Processor.hpp
+++ src/sakura/src/Processor.hpp
@@ -422,6 +422,8 @@ private:
   friend auto ROR_ABS(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   template <typename T>
   friend auto LSR_ZP_X(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
+  template <typename T>
+  friend auto ROL_ZP_X(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   // clang-format on
 
 public:
