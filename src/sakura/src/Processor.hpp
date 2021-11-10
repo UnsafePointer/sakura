efbc249dfc1cd91b62f92938c23203dc6533274f
Renzo Crisostomo <renzo@crisostomo.me>
2021-11-10 19:33:10 +0100

    Implement EOR (ABS, X)

--- src/sakura/src/Processor.hpp
+++ src/sakura/src/Processor.hpp
@@ -454,6 +454,8 @@ private:
   friend auto EOR_ABS(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   template <typename T>
   friend auto EOR_ZP_X(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
+  template <typename T>
+  friend auto EOR_ABS_X(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   // clang-format on
 
 public:
