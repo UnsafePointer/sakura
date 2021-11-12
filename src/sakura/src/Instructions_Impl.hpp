335d229a7ee7e3b1f69d95f8254d739660356b7c
Renzo Crisostomo <renzo@crisostomo.me>
2021-11-12 19:29:27 +0100

    Implement SBC (IND, Y)

--- src/sakura/src/Instructions_Impl.hpp
+++ src/sakura/src/Instructions_Impl.hpp
@@ -4105,4 +4105,52 @@ auto Sakura::HuC6280::ADC_IND_Y(std::unique_ptr<Processor> &processor,
   return 7;
 }
 
+template <>
+auto Sakura::HuC6280::SBC_IND_Y(std::unique_ptr<Processor> &processor,
+                                uint8_t opcode) -> uint8_t {
+  (void)opcode;
+  if (processor->m_registers.status.memory_operation &&
+      !processor->m_mos_6502_mode_enabled) {
+    spdlog::get(LOGGER_NAME)
+        ->critical("Unhandled SBC (IND, Y) with T flag set");
+    exit(1); // NOLINT(concurrency-mt-unsafe)
+  }
+  if (processor->m_registers.status.decimal) {
+    spdlog::get(LOGGER_NAME)
+        ->critical("Unhandled SBC (IND, Y) with D flag set");
+    exit(1); // NOLINT(concurrency-mt-unsafe)
+  }
+  uint8_t zp = processor->m_mapping_controller->load(
+      processor->m_registers.program_counter.value);
+  processor->m_registers.program_counter.value += 1;
+
+  uint16_t zp_address = processor->get_zero_page_address(zp);
+  uint16_t ll = processor->m_mapping_controller->load(zp_address);
+  uint16_t hh = processor->m_mapping_controller->load(zp_address + 1);
+
+  uint16_t address = hh << 8 | ll;
+  address += processor->m_registers.y;
+  uint8_t value = processor->m_mapping_controller->load(address);
+
+  value = ~value;
+
+  uint8_t result = processor->m_registers.accumulator + value +
+                   processor->m_registers.status.carry;
+  auto carry = static_cast<uint8_t>(
+      ((((uint16_t)processor->m_registers.accumulator & 0xFF) +
+        ((uint16_t)value & 0xFF) +
+        ((uint16_t)processor->m_registers.status.carry & 0x1)) &
+       0x100) == 0x100);
+  auto overflow = ((processor->m_registers.accumulator ^ result) &
+                   (value ^ result) & 0x80) != 0;
+  processor->m_registers.accumulator = result;
+
+  processor->m_registers.status.negative = (result >> 7) & 0b1;
+  processor->m_registers.status.overflow = overflow;
+  processor->m_registers.status.memory_operation = 0;
+  processor->m_registers.status.zero = result == 0;
+  processor->m_registers.status.carry = carry;
+  return 7;
+}
+
 #endif
