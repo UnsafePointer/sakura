f290793e671f814fa22040b0a07a0a3583d30a51
Renzo Crisostomo <renzo@crisostomo.me>
2021-11-12 19:17:48 +0100

    Implement SBC (IND, X)

--- src/sakura/src/Processor.hpp
+++ src/sakura/src/Processor.hpp
@@ -482,6 +482,8 @@ private:
   friend auto SBC_ABS_Y(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   template <typename T>
   friend auto ADC_IND_X(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
+  template <typename T>
+  friend auto SBC_IND_X(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   // clang-format on
 
 public:
