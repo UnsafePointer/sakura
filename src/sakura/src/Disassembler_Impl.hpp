6dd20f187d6fc64f2fa725ba28ba3fac11acca6f
Renzo Crisostomo <renzo@crisostomo.me>
2021-11-12 19:06:40 +0100

    Implement SBC (ZP, X)

--- src/sakura/src/Disassembler_Impl.hpp
+++ src/sakura/src/Disassembler_Impl.hpp
@@ -2432,4 +2432,18 @@ auto Sakura::HuC6280::ADC_ZP_X(std::unique_ptr<Processor> &processor,
           .length = 2};
 }
 
+template <>
+auto Sakura::HuC6280::SBC_ZP_X(std::unique_ptr<Processor> &processor,
+                               uint8_t opcode) -> Disassembled {
+  (void)opcode;
+  uint8_t zp = processor->m_mapping_controller->load(
+      processor->m_registers.program_counter.value);
+  zp += processor->m_registers.x;
+  uint16_t address = processor->get_zero_page_address(zp);
+  uint8_t value = processor->m_mapping_controller->load(address);
+  return {.mnemonic = fmt::format("SBC {:#04x}, X  @{:#06x}={:#04x}", zp,
+                                  address, value),
+          .length = 2};
+}
+
 #endif
--- src/sakura/src/Processor.hpp
+++ src/sakura/src/Processor.hpp
@@ -474,6 +474,8 @@ private:
   friend auto SBC_ABS(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   template <typename T>
   friend auto ADC_ZP_X(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
+  template <typename T>
+  friend auto SBC_ZP_X(std::unique_ptr<Processor> &processor, uint8_t opcode) -> T;
   // clang-format on
 
 public:
